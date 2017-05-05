8fa3b723d2336b2fba5af43ee3bc6802f9513a7c
Joseph Brasch <jbrasch@gmail.com>
2017-05-05 15:40:04 -0700

    Fix #267: Upgrade grunt-sass to one that builds on newer versions of NPM; also fix uglify option to allow building without error.

--- community-packages/Foundation/gruntfile.js
+++ community-packages/Foundation/gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           mangle: true,
-          compress: true
+          compress: {}
         },
         files: ['<%= jsFiles  %>']
       }
